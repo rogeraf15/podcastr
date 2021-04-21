94ab698d288a51b462f5037601b3276d197f9d63
Roger Fernandes <roger.afernandes@hotmail.com>
2021-04-21 09:49:26 -0300

    erro Image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,9 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
-
-          <title>podcastr</title>
         </Head>
         <body>
+          <title>podcastr</title>
           <Main />
           <NextScript />
         </body>
